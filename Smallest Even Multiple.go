4ecad75c1a954d7f172cdca355ce7ca36bf597f3
Michael Flanagan <mflanagan924@gmail.com>
2023-07-07 08:47:16 +0200

    added package main to Golang

--- Smallest Even Multiple.go
+++ Smallest Even Multiple.go
@@ -1,9 +1,11 @@
+package main
+
 func smallestEvenMultiple(n int) int {
-    //checks if the number is odd
-    if (n%2 > 0){
-        return n * 2;
-    //statement if the number is even
-    } else {
-        return n;
-    }
+	//checks if the number is odd
+	if n%2 > 0 {
+		return n * 2
+		//statement if the number is even
+	} else {
+		return n
+	}
 }
