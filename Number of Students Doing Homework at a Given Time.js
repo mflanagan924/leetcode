ad341e2be0db7bcffeaf705ce5845c19bd21b40c
Michael Flanagan <mflanagan924@gmail.com>
2024-01-15 10:19:06 +0100

    Number of Students Doing Homework at a Given Time

---
+++ Number of Students Doing Homework at a Given Time.js
@@ -0,0 +1,9 @@
+function busyStudent(startTime, endTime, queryTime){
+    let ans = 0;
+    for (let i = 0; i < startTime.length; i++){
+        if (startTime[i] <= queryTime && queryTime <= endTime[i]){
+            ans++;
+        }
+    }
+    return ans;
+}
