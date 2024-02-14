2fe762e9dfefc5df1b53929e761a6dffa12de718
Michael Flanagan <mflanagan924@gmail.com>
2024-02-14 10:27:50 +0100

    Airplane Seat Assignment Probability

---
+++ Airplane Seat Assignment Probability.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var nthPersonGetsNthSeat = function(n) {
+    if (n === 1){
+        return 1;
+    } else {
+        return 0.5;
+    }
+    
+};
