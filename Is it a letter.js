5f8202cc63a8e4120aaf52c3cc32273742266783
Michael Flanagan <mflanagan924@gmail.com>
2024-04-02 08:38:33 +0200

    Is it a letter?

---
+++ Is it a letter.js
@@ -0,0 +1,15 @@
+function isItLetter(character) {
+    let alphabet = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
+    'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
+    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 
+    'U', 'V', 'W', 'X', 'Y', 'Z'
+  ];
+    if (alphabet.indexOf(character) >= 0){
+      return true;
+    }
+    return false;
+    
+  }
