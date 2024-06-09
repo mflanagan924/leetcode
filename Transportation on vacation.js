179f4112027187675d58bf7060eefad969f9cd5d
Michael Flanagan <mflanagan924@gmail.com>
2024-06-09 07:41:48 +0200

    Transportation on vacation

---
+++ Transportation on vacation.js
@@ -0,0 +1,10 @@
+function rentalCarCost(d) {
+    let cost = d * 40;
+    if (d >= 7){
+        return cost - 50;
+    } else if (d >= 3){
+        return cost - 20;
+    } else {
+        return cost;
+    }
+  }
