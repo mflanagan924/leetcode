e5e2985de1b8a419de03ead618513ff83bf0f0f9
Michael Flanagan <mflanagan924@gmail.com>
2024-07-03 08:22:40 +0200

    Three Consecutive Odds

---
+++ Three Consecutive Odds.js
@@ -0,0 +1,14 @@
+function threeConsecutiveOdds(arr){
+    let count = 0;
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i]%2 !== 0){
+            count++;
+            if (count === 3){
+                return true;
+            }
+        } else {
+            count = 0;
+        }
+    }
+    return false;
+}
