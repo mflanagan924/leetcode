53006463382088049ecaca6e879bac4f671cb877
Michael Flanagan <mflanagan924@gmail.com>
2024-08-30 12:21:28 +0200

    Exclusive "or" (xor) Logical Operator

---
+++ Exclusive or (xor) Logical Operator.js
@@ -0,0 +1,11 @@
+function xor(a, b) {
+    if (a === true && b === false) {
+      return true;
+      }
+    else if (a === false && b === true){
+    return true;
+    }
+    else {
+      return false;
+      }
+  }
