1acc6f7db99224c995fb099a3e73bfa7763ecabc
Michael Flanagan <mflanagan924@gmail.com>
2024-11-08 09:26:01 +0100

    Opposites Attract

---
+++ Opposites Attract.js
@@ -0,0 +1,9 @@
+function lovefunc(flower1, flower2){
+    let first = flower1%2;
+    let second = flower2%2;
+    if (first !== second){
+      return true;
+    } else {
+      return false;
+    }
+  }
