3cdcd2765dfc17f4148eee5eff2589836b57a5ae
Michael Flanagan <mflanagan924@gmail.com>
2024-11-09 08:19:22 +0100

    Calculate average

---
+++ Calculate average.js
@@ -0,0 +1,10 @@
+function findAverage(array) {
+    if (array.length === 0) {
+      return 0;
+      }
+    let total = 0;
+    for (let i = 0; i < array.length; i++) {
+      total += array[i];
+      }
+    return total / array.length;
+  }
