cde134491925540c198a510ed9e06777240af4da
Michael Flanagan <mflanagan924@gmail.com>
2024-12-01 08:33:07 +0100

    A Strange Trip to the Market

---
+++ A Strange Trip to the Market.js
@@ -0,0 +1,7 @@
+function isLockNessMonster(s) {
+    if (s.indexOf("tree fiddy") >= 0 || s.indexOf("3.50") >= 0 || s.indexOf("three fifty") >= 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
