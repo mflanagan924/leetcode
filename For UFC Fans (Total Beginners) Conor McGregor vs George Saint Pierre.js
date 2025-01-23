51c79ca9c8b1c19235f2a0fb28809a1970b38f51
Michael Flanagan <mflanagan924@gmail.com>
2025-01-23 09:11:28 +0100

    For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre

---
+++ For UFC Fans (Total Beginners) Conor McGregor vs George Saint Pierre.js
@@ -0,0 +1,8 @@
+function quote(fighter) {
+    let name = fighter.toLowerCase();
+    if (name === "george saint pierre"){
+      return "I am not impressed by your performance.";
+    } else {
+      return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+    }
+  };
