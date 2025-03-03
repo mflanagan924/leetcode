de204335559319285dfa8f096f8feb76ac901d07
Michael Flanagan <mflanagan924@gmail.com>
2025-03-03 09:24:10 +0100

    Find twins

---
+++ Find twins.js
@@ -0,0 +1,10 @@
+function elimination(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] === arr[j]) {
+                return arr[i];
+            }
+        }
+    }
+    return null;
+}
